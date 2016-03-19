1fbb2966949f220147e431f79f99a029b2be611a
ajean17 <AlvinDeCode@yahoo.com>
2016-03-19 17:30:40 -0400

    Finished up the enemy script for now

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
     public int playerDamage;                           
 
 
@@ -11,15 +12,61 @@ public class Enemy : MonoBehaviour {
 
     public void MoveEnemy()
     {
+        int xDir = 0;
+        int yDir = 0;
+
+        //If the difference in positions is approximately zero (Epsilon) do the following:
+        if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
+
+            //If the y coordinate of the target's (player) position is greater than the y coordinate of this enemy's position set y direction 1 (to move up). If not, set it to -1 (to move down).
+            yDir = target.position.y > transform.position.y ? 1 : -1;
+
+        //If the difference in positions is not approximately zero (Epsilon) do the following:
+        else
+            //Check if target x position is greater than enemy's x position, if so set x direction to 1 (move right), if not set to -1 (move left).
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+
+        //Call the AttemptMove function and pass in the generic parameter Player, because Enemy is moving and expecting to potentially encounter a Player
+        AttemptMove<Player>(xDir, yDir);
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        //Check if skipMove is true, if so set it to false and skip this turn.
+        if (skipMove)
+        {
+            skipMove = false;
+            return;
+        }
+
+        //Call the AttemptMove function from MovingObject.
+        base.AttemptMove<T>(xDir, yDir);
+
+        //Now that Enemy has moved, set skipMove to true to skip next move.
+        skipMove = true;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        base.Start();
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        //Declare hitPlayer and set it to equal the encountered component.
+        Player hitPlayer = component as Player;
+
+        //Call the LoseFood function of hitPlayer passing it playerDamage, the amount of foodpoints to be subtracted.
+        hitPlayer.LoseFood(playerDamage);
 
     }
-    // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update ()
+    {
 	
 	}
 }
