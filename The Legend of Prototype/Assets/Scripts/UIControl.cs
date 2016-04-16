97d42f0e2cc6aa553b211959517f49dfd5f4c6a1
hailey montgomery <hmontgomery@ggc.edu>
2016-04-16 14:29:43 -0400

    (Buggy?) Title Screen

---
+++ The Legend of Prototype/Assets/Scripts/UIControl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+
+public class UIControl : MonoBehaviour {
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
